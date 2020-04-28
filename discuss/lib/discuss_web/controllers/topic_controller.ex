b4fb0fa210778e8d383c5905433125eec190d0b6
broem <wacbleach@gmail.com>
2020-04-27 21:24:43 -0400

    editing and routes and junk

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -24,7 +24,7 @@ defmodule DiscussWeb.TopicController do
         changeset = Topic.changeset(%Topic{}, topic)
 
         case Discuss.Repo.insert(changeset) do
-            {:ok, post} ->
+            {:ok, _topic} ->
                 conn
                 |> put_flash(:info, "Topic Created")
                 |> redirect(to: Routes.topic_path(conn, :index))
@@ -32,6 +32,31 @@ defmodule DiscussWeb.TopicController do
                 conn
                 |> put_flash(:error, "Error")
                 render conn, "new.html", changeset: changeset
+        end
+    end
+
+    def edit(conn, %{"id" => topic_id}) do
+        topic = Discuss.Repo.get(Topic, topic_id)
+        changeset = Topic.changeset(topic)
+
+        render conn, "edit.html", changeset: changeset, topic: topic
+    end
+
+    def update(conn, %{"id" => topic_id, "topic" => topic}) do
+        # old_topic = Discuss.Repo.get(Topic, topic_id)
+        # changeset = Topic.changeset(old_topic, topic)
+
+        changeset = Discuss.Repo.get(Topic, topic_id) |> Topic.changeset(topic)
+
+        case Discuss.Repo.update(changeset) do
+            {:ok, _topic} ->
+                conn
+                |> put_flash(:info, "Topic Updated")
+                |> redirect(to: Routes.topic_path(conn, :index))
+            {:error, changeset} ->
+                conn
+                |> put_flash(:error, "Error")
+                render conn, "edit.html", changeset: changeset
         end
     end
 end
